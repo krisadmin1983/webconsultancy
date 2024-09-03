f5630916b2ced1ea194a1010438339f62c4ce2d6
krisadmin1983 <128992081+krisadmin1983@users.noreply.github.com>
2024-09-03 09:52:25 +0800

    Add files via upload

---
+++ consultancy/consultancy/urls.py
@@ -0,0 +1,8 @@
+# consultancy/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),  # Ensure this line is correct
+]
